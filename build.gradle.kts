66ae4f76e35c30097e3286b51ff9bd869d31cbd6
Saud Khan <dev@bidyut.com>
2024-11-15 21:52:51 +0800

    Ensure same compose plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.parcelize).apply(false)
     alias(libs.plugins.jetbrains.kotlin.serialization).apply(false)
     alias(libs.plugins.jetbrains.kotlinx.kover).apply(false)
+    alias(libs.plugins.jetbrains.compose).apply(false)
     alias(libs.plugins.ktorfit).apply(false)
 }
 
